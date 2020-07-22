2a963b8e725e08939c4c3b4b38f82c41c22d71cf
Zach Osman <zosman@gmu.edu>
2020-07-22 15:27:22 -0400

    slight changes to test file

--- tests/test.js
+++ tests/test.js
@@ -3,47 +3,47 @@ var assert = require("assert");
 // import words from "../dist/index";
 const { words } = require("../dist/index");
 
-describe("random-words", function() {
-  it("should return one word when called with no arguments", function() {
+describe("random-words", function () {
+  it("should return one word when called with no arguments", function () {
     const word = words();
-    console.warn("-1 word-");
-    console.warn(word);
+    console.log("-1 word-");
+    console.log(word);
     assert.ok(typeof word === "string", "word is a string");
     assert.ok(word.length, "word is not empty");
     assert.ok(word.indexOf(" ") === -1, "word does not contain spaces");
   });
 
-  it("should return 2 words in camelCase", function() {
+  it("should return 2 words in camelCase", function () {
     const word = words(2, "camelCase");
-    console.warn("-2 word camelCase-");
-    console.warn(word);
+    console.log("-2 word camelCase-");
+    console.log(word);
     assert.ok(typeof word === "string", "word is a string");
     assert.ok(word.length, "word is not empty");
     assert.ok(word.indexOf(" ") === -1, "word does not contain spaces");
   });
 
-  it("should return 2 words in kebabCase", function() {
+  it("should return 2 words in kebabCase", function () {
     const word = words(2, "kebabCase");
-    console.warn("-2 word kebab-case-");
-    console.warn(word);
+    console.log("-2 word kebab-case-");
+    console.log(word);
     assert.ok(typeof word === "string", "word is a string");
     assert.ok(word.length, "word is not empty");
     assert.ok(word.indexOf(" ") === -1, "word does not contain spaces");
   });
 
-  it("should return 2 words in snake_case", function() {
+  it("should return 2 words in snake_case", function () {
     const word = words(2, "snakeCase");
-    console.warn("-2 word snake_case-");
-    console.warn(word);
+    console.log("-2 word snake_case-");
+    console.log(word);
     assert.ok(typeof word === "string", "word is a string");
     assert.ok(word.length, "word is not empty");
     assert.ok(word.indexOf(" ") === -1, "word does not contain spaces");
   });
 
-  it("should return 5 words in camelCase", function() {
+  it("should return 5 words in camelCase", function () {
     const word = words(5, "camelCase");
-    console.warn("-2 word camelCase-");
-    console.warn(word);
+    console.log("-5 word camelCase-");
+    console.log(word);
     assert.ok(typeof word === "string", "word is a string");
     assert.ok(word.length, "word is not empty");
     assert.ok(word.indexOf(" ") === -1, "word does not contain spaces");
